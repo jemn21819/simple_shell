858c60d274259b719c24556ac01cb3993fa67a49
Jaime Martinez <jemn1jemn@gmail.com>
2020-11-16 11:27:48 +0000

    adding new functions

--- holberton.h
+++ holberton.h
@@ -11,18 +11,23 @@
 #include <string.h>
 
 
+/* print string version */
+int _printstr(char *str);
+
+/* string helper functions */
 int _strlen(char *s);
-
 void _strcpy(char *dest, char *src, size_t n);
-
 char *_strdup(char *str);
-
-void free_pointers(char **pointers);
-
-char **line_split(char *line);
-
 int _strcmp(char *s1, char *s2);
-
 int _str_n_cmp(char *s1, const char *s2, int n);
+
+/* tokenizer functions */
+char get_delim(const char c, const char *delim);
+size_t tok_counter(const char *line, const char *delim);
+void tok_string(char **arstr, char *line, const char *delim);
+char **tokenizer(char *line, const char *delim);
+
+
+
 
 #endif
