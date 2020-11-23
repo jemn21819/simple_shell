d3b67a86df94248ab62ecb8fe7c0d2641540b16e
Jaime Martinez <jemn1jemn@gmail.com>
2020-11-23 22:10:33 +0000

    Fixing leak problem

--- main.c
+++ main.c
@@ -1,45 +1,47 @@
 #include "holberton.h"
 
 /**
- * main - command line interpreter and start point of program
- * @ac: arg counter
- * @av: arg vector
- * @env: pointer to user enviroment
- * Return: status
+ * main - Display prompt and get a line and initialice process
+ * Return: Always 0.
  */
 
-int main(int  ac, char **av, char **env)
+int main(void)
 {
-	char *line;
-	char **args;
-	size_t size;
-	int status;
+	char *prompt = "$ ", *line = NULL, *tk = NULL, *av[256];
+	size_t bufsize = 1024;
+	pid_t pid;
+	int i = 0, j;
 
-	signal(SIGINT, handler);
-	while (1)
-	{
-		line = NULL;
-		size = 0;
-
+	do {
 		if (isatty(STDIN_FILENO) == 1)
-			_printstr("$ ");
-
-		if (getline(&line, &size, stdin) == EOF)
+			write(STDOUT_FILENO, prompt, 2);
+		signal(SIGINT, handler);
+		i = getline(&line, &bufsize, stdin);
+		if (i == EOF)
 		{
-			if (isatty(0) == 1)
-				_printstr("\n");
-			break;
+			(isatty(STDIN_FILENO) == 1)
+				write(STDOUT_FILENO, "\n", 1);
+			return ((free(line), 0);
 		}
-
-		args = tokenizer(line, " \n\t");
-		if (args != NULL)
+		if (_strcmp(line, "exit\n") == 0)
+			return ((free(line), 0);
+		
+		pid = fork();
+		if (pid == -1)
+			return (1);
+		if (pid == 0)
 		{
-			status = exe_cmd(args, env);
+			for (j = 0, tk = strtok(line, " \n"); tk; tk = strtok(NULL, " \n"), j++)
+				av[j] = tk;
+			av[j] = '\0';
+			if (av[0][0] != '.')
+				path(&av[0]);
+			if (execve(av[0], av, NULL) == -1)
+				perror("Error:");
+			exit(0);
 		}
-		free(line);
-		free(args);
-	}
-	(void)ac;
-	(void)av;
-	return (status);
+		else
+			wait(NULL);
+	} while (1);
+	return (0);
 }
