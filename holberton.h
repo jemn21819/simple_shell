80e914f15bdac2370b6331da9fb91679683363cb
Jeff Martinez <2408@holbertonschool.com>
2020-11-24 02:10:46 +0000

    Updating the header.

--- holberton.h
+++ holberton.h
@@ -12,18 +12,36 @@
 
 extern char **environ;
 
-/* string helper functions */
+/* helping functions */
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
-char *_strchr(char *s, char c);
-char *_strstr(char *src, char *sub);
-char *str_concat(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strcat(char *s1, char *s2);
+int _intlen(int n);
+int _atoi(char *str);
+char *_strdup(char *s1);
+char *itoa(int n);
+void _printf(char *str);
+int _putchar(char c);
 
-/* env and path */
+void mem_set(void *mem, int byte, size_t n);
+char *get_env_var_val(char *env_var);
+char *get_full_comd_path(char **tokens, char *env_var_val);
+void free_str_array(char **array);
+char **parse_path(char *line, char);
+int count_chars(char *s1, char seperator);
+int count_words(char *s, char seperator);
 
-char *_getenv(char *var);
-void path(char **argv);
-void _env(void);
+int count_chars(char *line, char space);
+int count_tokens(char *line);
+int exec_bltins_prgrms(char **tokens, char **env, int status, char *line);
+int exit_shell(char **tokens, char *line);
+int _env(char **env);
+int exec_prgrms(char **tokens, char *line);
+char **_strtok(char *line);
+void prompt(void);
+char *_getline(void);
+
 
 /* handler for ctrl+c signal */
 void handler(int signal);
